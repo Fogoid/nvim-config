ac292567d21ef2370bfaf050068cce758df4f68e
Diogo Fernandes <diogo.barradas.fernandes@gmail.com>
2023-09-20 11:56:04 +0100

    autocomplete with tab

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -34,14 +34,14 @@ lspconfig.omnisharp.setup{
     organize_imports_on_format = true
 }
 
---lspconfig.rust_analyzer.setup{}
+lspconfig.rust_analyzer.setup{}
 
 local cmp = require('cmp')
 local cmp_select = {behavior = cmp.SelectBehavior.Select}
 local cmp_mappings = lsp.defaults.cmp_mappings({
   ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
   ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-  ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+  ['<TAB>'] = cmp.mapping.confirm({ select = true }),
   ["<C-Space>"] = cmp.mapping.complete(),
 })
 
