5d47f9949a69238509940ae47a9f601033ea7366
x802695 <x802695@servexternos.gruposantander.com>
2023-10-02 10:14:15 +0100

    Remap window movement

--- lua/fogoid/remap.lua
+++ lua/fogoid/remap.lua
@@ -9,3 +9,7 @@ vim.keymap.set("n", "<C-u>", "<C-u>zz")
 -- greatest remap ever
 vim.keymap.set("x", "<leader>p", [["_dP]])
 
+vim.keymap.set("n", "<leader>wl", "<C-w>l")
+vim.keymap.set("n", "<leader>wh", "<C-w>h")
+vim.keymap.set("n", "<leader>wj", "<C-w>j")
+vim.keymap.set("n", "<leader>wk", "<C-w>k")
