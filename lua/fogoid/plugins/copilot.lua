eeb59620908ba296ef0382b78a6d31c02114eaa1
Fogoid <diogo.barradas.fernandes@gmail.com>
2024-03-12 23:21:58 +0000

    try tabnine instead of copilot

--- lua/fogoid/plugins/copilot.lua
+++ lua/fogoid/plugins/copilot.lua
@@ -1,10 +1,18 @@
 return {
-    'github/copilot.vim',
-    init = function ()
-        vim.keymap.set('i', '<C-J>', 'copilot#Accept("<CR>")', {
-          expr = true,
-          replace_keycodes = false
-        })
-        vim.g.copilot_no_tab_map = true
-    end
+    {
+        'codota/tabnine-nvim',
+        build = "./dl_binaries.sh",
+        config = function()
+            require('tabnine').setup({
+                disable_auto_comment = true,
+                accept_keymap = "<C-j>",
+                dismiss_keymap = "<C-]>",
+                debounce_ms = 800,
+                suggestion_color = { gui = "#808080", cterm = 244 },
+                exclude_filetypes = { "TelescopePrompt", "NvimTree" },
+                log_file_path = nil, -- absolute path to Tabnine log file
+            })
+        end
+    },
+
 }
